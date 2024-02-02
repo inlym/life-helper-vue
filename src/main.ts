39d1bbc72200c13c221b7dca87b97468e07e48cb
inlym <inlym@qq.com>
2024-02-02 23:19:39 +0800

    :heavy_minus_sign: 移除 `Ant Design Vue`

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import './assets/main.css'
 
-import {createApp} from 'vue'
 import {createPinia} from 'pinia'
-import Antd from 'ant-design-vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-// 全局完整注册 Ant Design Vue
-app.use(Antd)
 
 app.use(createPinia())
 app.use(router)
