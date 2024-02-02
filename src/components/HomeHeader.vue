e5b5821b085487dc6c96ab8c1a5ab0eaa0c5f11b
inlym <inlym@qq.com>
2024-02-02 22:18:23 +0800

    :construction: 基础文件修改

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  border: 1px solid red;
+  width: 100%;
+  height: 80px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
+<template>
+  <HomeHeader></HomeHeader>
+  <div class="main"></div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeHeader from '@/components/HomeHeader.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped lang="scss">
+.main {
+  border: 1px solid blue;
+  width: 100%;
+  height: 500px;
+}
+</style>
