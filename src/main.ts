28403b9f6f451aaf92377b9a44f66fb60919f285
inlym <inlym@qq.com>
2024-02-02 22:55:32 +0800

    :heavy_plus_sign: 引入 `Ant Design Vue`

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+// 全局完整注册 Ant Design Vue
+app.use(Antd)
 
 app.use(createPinia())
 app.use(router)
