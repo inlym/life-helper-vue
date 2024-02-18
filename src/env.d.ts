6fda5899add758ccb160403b6f2b461f3ddf33c0
inlym <inlym@qq.com>
2024-02-19 00:18:36 +0800

    :sparkles: 引入百度统计

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,10 @@ interface ImportMetaEnv {
   readonly VITE_APP_NAME: string
   /** 服务器请求地址 */
   readonly VITE_REQUEST_BASE_URL: string
+  /** 是否开启百度统计 */
+  readonly VITE_BAIDU_ENABLED: string
+  /** 百度统计网站 ID */
+  readonly VITE_BAIDU_WEBSITE_ID: string
 }
 
 interface ImportMeta {
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import './assets/main.css'
 
 import {createPinia} from 'pinia'
 import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-
-// Vuetify
-import 'vuetify/styles'
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import App from './App.vue'
+import {baiduEnable, baiduWebsiteId, registerBaiduAnalytics} from './core/analytics'
+import router from './router'
 
 const vuetify = createVuetify({components, directives})
 
@@ -17,5 +16,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+
+// 仅在线上环境开启百度统计
+if (baiduEnable) {
+  app.use(registerBaiduAnalytics, {
+    router,
+    websiteIds: [baiduWebsiteId],
+    debug: false
+  })
+}
 
 app.mount('#app')
