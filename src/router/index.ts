582880af4717af6853fba7695d11e285392306ac
inlym <inlym@qq.com>
2024-02-20 23:31:38 +0800

    :egg: 新增临时调试页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import {authGuard} from '@/core/guards/auth-guard'
+import TestView from '@/views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,6 +31,12 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: {requireAuth: false}
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView,
       meta: {requireAuth: false}
     }
   ]
---
+++ src/views/TestView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>临时调试用途页面</h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
