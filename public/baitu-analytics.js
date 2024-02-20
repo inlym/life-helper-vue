3b4a3d33bd0a8cf8d19e8c5b9f8c475fe2c35206
inlym <inlym@qq.com>
2024-02-20 19:36:05 +0800

    :building_construction: 移除 `@web-analytics/vue` 依赖，使用原生方式使用百度统计（已原生支持 SPA）

---
+++ public/baitu-analytics.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+var _hmt = _hmt || []
+  ; (function () {
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?0040b477cfd5ba8feb5440d06b4fe3de'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
+  })()
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import App from './App.vue'
-import {baiduEnable, baiduWebsiteId, registerBaiduAnalytics} from './core/analytics'
 import router from './router'
 
 const vuetify = createVuetify({components, directives})
@@ -16,14 +15,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-
-// 仅在线上环境开启百度统计
-if (baiduEnable) {
-  app.use(registerBaiduAnalytics, {
-    router,
-    websiteIds: [baiduWebsiteId],
-    debug: false
-  })
-}
 
 app.mount('#app')
