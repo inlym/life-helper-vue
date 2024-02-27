4088589b2a3e2f1593f345e8722c33faa742041f
inlym <inlym@qq.com>
2024-02-28 01:33:37 +0800

    :building_construction: 定制自定义主题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <RouterView />
+  <a-config-provider :theme="myTheme">
+    <RouterView />
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
 import {RouterView} from 'vue-router'
+import {ref} from 'vue'
+
+/** 自定义主题配置 */
+const myTheme = ref({
+  token: {
+    colorPrimary: '#00b669'
+  }
+})
 </script>
