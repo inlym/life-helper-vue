a422a0e60873ab8dad1fae628546f8461cc98a27
inlym <inlym@qq.com>
2024-03-01 00:26:27 +0800

    :sparkles: 完成扫码登录页

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,17 +8,92 @@
       <a-typography-text class="desc" type="secondary">使用微信扫一扫登录</a-typography-text>
       <!-- 二维码区域 -->
       <div class="qrcode-area">
-        <img class="qrcode" :src="data?.url" alt="" srcset="" />
+        <!-- 二维码图片 -->
+        <a-spin :spinning="loading" size="large"><img class="qrcode" :src="data.url" /></a-spin>
+        <!-- 已扫码样式 -->
+        <div v-if="status === 2" class="upper">
+          <div class="mask"></div>
+          <a-flex class="box" vertical justify="space-between" align="center">
+            <CheckCircleFilled class="icon" style="font-size: 60px; color: #00b367" /> <a-typography-text class="desc" :level="5">扫码成功</a-typography-text>
+          </a-flex>
+        </div>
+        <!-- 已失效样式 -->
+        <div v-if="status === 4" class="upper">
+          <div class="mask"></div>
+          <a-flex class="box" vertical justify="space-between" align="center">
+            <InfoCircleFilled class="icon" style="font-size: 60px; color: #ffc300" />
+            <div class="desc">
+              <a-typography-text :level="5">已失效，</a-typography-text>
+              <a-typography-text class="refresh" :level="5" @click="refresh">点此刷新</a-typography-text>
+            </div>
+          </a-flex>
+        </div>
       </div>
     </a-flex>
   </a-flex>
 </template>
 
 <script setup lang="ts">
-import {getQrCodeInfo} from '@/services/login'
+import {saveIdentityCertificate} from '@/core/auth'
+import {checkLoginResult, getQrCodeInfo} from '@/services/login'
 import {useRequest} from 'alova'
+import {ref} from 'vue'
+import {CheckCircleFilled, InfoCircleFilled} from '@ant-design/icons-vue'
 
-const {loading, data, error} = useRequest(getQrCodeInfo)
+/**
+ * 扫码登录状态
+ *
+ * ### 说明
+ * 扫码登录凭据的各个状态的处理策略：
+ * 1. [1] -> 被扫码端处理：不变
+ * 2. [2] -> 被扫码端处理：增加已扫码样式
+ * 3. [3] -> 会包含 IdentityCertificate -> 被扫码端处理：登录成功，进行页面跳转，并且不再继续轮询查询登录结果
+ * 4. [4] -> 被扫码端处理：增加已失效样式，并且不再继续轮询查询登录结果
+ */
+const status = ref<number>(1)
+
+/** 是否继续轮询 */
+const polling = ref(true)
+
+const {data, onSuccess, loading} = useRequest(getQrCodeInfo, {initialData: {id: '', url: ''}})
+
+const pollingTask = (id: string) => {
+  const timer = setInterval(() => {
+    checkLoginResult(id).then((res) => {
+      if (res.status === 1 || res.status === 2) {
+        status.value = res.status
+      } else if (res.status === 3) {
+        // 登录成功情况
+        polling.value = false
+        saveIdentityCertificate(res.certificate)
+        clearInterval(timer)
+
+        // TODO: 登录成功后的后续操作
+        // ...
+      } else if (res.status === 4) {
+        status.value = res.status
+        polling.value = false
+        clearInterval(timer)
+      }
+    })
+  }, 1000)
+}
+
+onSuccess(() => {
+  pollingTask(data.value.id)
+})
+
+/** 刷新二维码资源 */
+const refresh = () => {
+  getQrCodeInfo().then((res) => {
+    console.log(res)
+
+    data.value = res
+    status.value = 1
+    polling.value = true
+    pollingTask(res.id)
+  })
+}
 </script>
 
 <style scoped lang="scss">
@@ -50,15 +125,45 @@ const {loading, data, error} = useRequest(getQrCodeInfo)
     .qrcode-area {
       position: relative;
       padding: 10px;
-      margin-top: 20px;
+      margin-top: 60px;
       border: 1px solid #ddd;
       border-radius: 4px;
       height: 200px;
       width: 200px;
 
       .qrcode {
+        z-index: 1;
         height: 180px;
         width: 180px;
+      }
+      .upper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        .mask {
+          position: absolute;
+          top: 0;
+          left: 0;
+          backdrop-filter: blur(50px);
+          height: 198px;
+          width: 198px;
+          z-index: 10;
+        }
+        .box {
+          padding: 50px 0;
+          position: absolute;
+          height: 198px;
+          width: 198px;
+          top: 0;
+          left: 0;
+          z-index: 99;
+
+          .desc {
+            .refresh {
+              color: #576b95;
+            }
+          }
+        }
       }
     }
   }
