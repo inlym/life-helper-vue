108dc353f3d673f8668877419454dc4817552077
inlym <inlym@qq.com>
2024-02-29 20:54:50 +0800

    :wrench: 修改百度统计配置

--- public/js/baitu-analytics.js
+++ public/js/baitu-analytics.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || []
   ; (function () {
     var hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?0040b477cfd5ba8feb5440d06b4fe3de'
+    hm.src = 'https://hm.baidu.com/hm.js?2736610af329cd8cf279c0b314d96f06'
     var s = document.getElementsByTagName('script')[0]
     s.parentNode.insertBefore(hm, s)
   })()
