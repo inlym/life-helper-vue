4b95eec7a5ac0f996ddfd244a89cb5af5086fecb
inlym <inlym@qq.com>
2024-08-07 23:13:43 +0800

    chore: init project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -20,4 +18,4 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
