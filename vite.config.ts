6dd5c9d403109cbc5b96f72550229f3042f4a825
inlym <inlym@qq.com>
2024-08-07 22:58:04 +0800

    chore: introduce environment variable configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  // 环境变量目录
+  // https://cn.vitejs.dev/config/shared-options.html#envdir
+  envDir: path.resolve(__dirname, './env')
 })
