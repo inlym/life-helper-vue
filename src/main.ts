783b4f1cb31ad0fdc1382bcce3055ffc34d97051
inlym <inlym@qq.com>
2024-08-16 19:00:53 +0800

    chore: remove ant design vue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import Antd from 'ant-design-vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App)
 
@@ -12,9 +9,5 @@ app.use(createPinia())
 
 // 注册路由
 app.use(router)
-
-// 对 Ant Design Vue 进行全局完整注册
-// https://antdv.com/docs/vue/getting-started-cn
-app.use(Antd)
 
 app.mount('#app')
