f5c2d4a180852f846e3eb74234091f80537baba9
inlym <inlym@qq.com>
2024-08-26 00:00:05 +0800

    chore: install vuetify dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,11 @@ import router from './router'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
-// 注册 Vuetify 组件库
-app.use(
-  createVuetify({
-    components,
-    directives
-  })
-)
+// 注册 Vuetify
+app.use(createVuetify())
 
 // 注册 Pinia
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  // Vuetify 插件
+  // https://vuetifyjs.com/zh-Hans/features/treeshaking
+  plugins: [vue(), vuetify(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
