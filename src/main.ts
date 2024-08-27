0f58bf1dd4cd4141cbf9a6c59d6bb92061fd455b
inlym <inlym@qq.com>
2024-08-28 00:15:49 +0800

    chore: install `@mdi/font`

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import {createPinia} from 'pinia'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {aliases, mdi} from 'vuetify/iconsets/mdi'
 
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import {createVuetify} from 'vuetify'
 
 const app = createApp(App)
 
 // 注册 Vuetify
-app.use(createVuetify())
+app.use(
+  createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {mdi},
+    },
+  }),
+)
 
 // 注册 Pinia
 app.use(createPinia())
