d2eebdfaffc640d6e9746ecaac6ceafcf59cbecd
inlym <inlym@qq.com>
2024-08-30 21:28:29 +0800

    perf: optimize `createAliyunApigwSignatureInterceptor` function

--- src/core/axios/aliyunApigwSignatureInterceptor.ts
+++ src/core/axios/aliyunApigwSignatureInterceptor.ts
@@ -1,17 +1,9 @@
-import {type InternalAxiosRequestConfig} from 'axios'
+import {type AxiosHeaderValue, type InternalAxiosRequestConfig} from 'axios'
 import Base64 from 'crypto-js/enc-base64'
 import Hex from 'crypto-js/enc-hex'
 import hmacSHA256 from 'crypto-js/hmac-sha256'
 import md5 from 'crypto-js/md5'
 import {paramsSerializer} from './paramsSerializer'
-
-/** 空字符串 */
-const EMPTY_STRING = ''
-
-/** 换行符 */
-const LF = '\n'
-
-const DEFAULT_ACCEPT = '*/*'
 
 /**
  * 阿里云 API 网关签名拦截器
@@ -29,31 +21,32 @@ export function createAliyunApigwSignatureInterceptor(appKey: string, appSecret:
       'date',
     ]
 
+    /** 换行符 */
+    const LF = '\n'
+
     const headers = config.headers
 
     // 处理 `Accept`: 为空时赋个默认值
-    headers.setAccept(DEFAULT_ACCEPT, false)
+    headers.setAccept('*/*', false)
 
     // 处理 `Content-Type` 和 `Content-Md5`
-    if (['post', 'put', 'patch'].includes(config.method!.toLowerCase()) && config.data !== undefined) {
+    if (
+      ['post', 'put', 'patch'].includes(config.method!.toLowerCase()) &&
+      config.data !== undefined &&
+      config.data !== null
+    ) {
       if (typeof config.data === 'object') {
         if (config.data instanceof FormData) {
           throw new Error('暂不支持 FormData 形式的请求数据')
         } else {
           headers.setContentType('application/json')
-        }
-      } else if (typeof config.data === 'string') {
-        if (config.data.includes('=')) {
-          headers.setContentType('application/x-www-form-urlencoded')
-        } else {
-          headers.setContentType('text/plain')
+          headers.set('content-md5', md5(JSON.stringify(config.data)).toString(Base64))
         }
       } else {
         // 保底策略
         headers.setContentType('text/plain')
+        headers.set('content-md5', md5(String(config.data)).toString(Base64))
       }
-
-      headers.set('content-md5', md5(JSON.stringify(config.data)).toString(Base64))
     }
 
     // 添加认证需要的必要请求头
@@ -67,13 +60,15 @@ export function createAliyunApigwSignatureInterceptor(appKey: string, appSecret:
     const signedHeaders: string[] = []
 
     signedStrings.push(config.method!.toUpperCase())
-    signedStrings.push(headers.getAccept() ? String(headers.getAccept()) : EMPTY_STRING)
-    signedStrings.push(headers.get('content-md5') ? String(headers.get('content-md5')) : EMPTY_STRING)
-    signedStrings.push(headers.getContentType() ? String(headers.getContentType()) : EMPTY_STRING)
-    signedStrings.push(headers.get('date') ? String(headers.get('date')) : EMPTY_STRING)
+    signedStrings.push(stringifyHeaderValue(headers.getAccept()))
+    signedStrings.push(stringifyHeaderValue(headers.get('content-md5')))
+    signedStrings.push(stringifyHeaderValue(headers.getContentType()))
+    // 这一项取 Date 的值，由于不能编程对其赋值，直接特殊处理为空
+    signedStrings.push('')
 
     Object.keys(headers)
-      .filter((key) => !EXCLUDED_SIGNATURE_HEADERS.includes(key.toLowerCase()))
+      .map((key) => key.toLowerCase())
+      .filter((key) => !EXCLUDED_SIGNATURE_HEADERS.includes(key) && !isForbiddenHeader(key))
       .sort()
       .forEach((key) => {
         signedStrings.push(key.toLowerCase() + ':' + headers.get(key))
@@ -96,5 +91,51 @@ export function createAliyunApigwSignatureInterceptor(appKey: string, appSecret:
     }
 
     return config
+  }
+}
+
+/** 判断是否是禁止修改的请求头 */
+export function isForbiddenHeader(headerName: string): boolean {
+  /** 禁止修改的请求头 */
+  const FORBIDDEN_HEADERS = [
+    'accept-charset',
+    'accept-encoding',
+    'access-control-request-headers',
+    'access-control-request-method',
+    'connection',
+    'content-length',
+    'cookie',
+    'date',
+    'dnt',
+    'expect',
+    'permissions-policy',
+    'host',
+    'keep-alive',
+    'origin',
+    'referer',
+    'te',
+    'trailer',
+    'transfer-encoding',
+    'upgrade',
+    'via',
+    'user-agent',
+  ]
+
+  return (
+    FORBIDDEN_HEADERS.includes(headerName.toLowerCase()) ||
+    headerName.toLowerCase().startsWith('proxy-') ||
+    headerName.toLowerCase().startsWith('sec-')
+  )
+}
+
+/** 将请求头的字段值转为字符串 */
+export function stringifyHeaderValue(value: AxiosHeaderValue): string {
+  /** 空字符串 */
+  const EMPTY_STRING = ''
+
+  if (typeof value === 'undefined' || value === null) {
+    return EMPTY_STRING
+  } else {
+    return String(value)
   }
 }
--- src/core/axios/authInterceptor.ts
+++ src/core/axios/authInterceptor.ts
@@ -1,6 +1,8 @@
-import type { InternalAxiosRequestConfig } from 'axios'
+import type {InternalAxiosRequestConfig} from 'axios'
 
 /** 鉴权拦截器 */
 export function authInterceptor(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
+  console.log(config.headers)
+
   return config
 }
