11818852317e5cd702078f62826f40cb9a526948
inlym <inlym@qq.com>
2024-09-03 00:18:06 +0800

    perf: optimize `handleBusinessError`

--- src/core/axios/handleBusinessError.ts
+++ src/core/axios/handleBusinessError.ts
@@ -1,10 +1,13 @@
 import {type AxiosResponse} from 'axios'
 import {BusinessError, type CommonResponse} from '../types'
+import {logger} from '../logger'
 
 /** 封装处理业务错误 */
 export function handleBusinessError(response: AxiosResponse<CommonResponse>): AxiosResponse {
   const data = response.data
   if (data.errorCode && data.errorCode > 0) {
+    logger.error(`[HTTP ERROR] errorCode=${data.errorCode}, errorMessage=${data.errorMessage}`)
+
     throw new BusinessError(data)
   }
 
