89937979882f2fe827845024b3622964da05d867
inlym <inlym@qq.com>
2024-09-03 01:00:23 +0800

    feat: optimize GlobalDialog

--- src/components/GlobalDialog.vue
+++ src/components/GlobalDialog.vue
@@ -1,9 +1,15 @@
 <template>
-  <v-dialog v-model="globalDialog.dialog" width="auto" persistent>
-    <v-card width="400" :text="globalDialog.text" title="提示">
+  <v-dialog v-model="dialog.show" width="auto" persistent>
+    <v-card width="400" :text="dialog.text" :title="dialog.title">
       <template v-slot:actions>
         <v-spacer />
-        <v-btn class="ms-auto" text="我知道了" @click="globalDialog.hideDialog()"></v-btn>
+        <v-btn v-if="dialog.showCancel" class="ms-auto" :text="dialog.cancelText" @click="dialog.hideDialog"></v-btn>
+        <v-btn
+          class="ms-auto"
+          :color="dialog.confirmButtonColor"
+          :text="dialog.confirmText"
+          @click="dialog.hideDialog"
+        ></v-btn>
       </template>
     </v-card>
   </v-dialog>
@@ -12,7 +18,7 @@
 <script setup lang="ts">
 import {useDialog} from '@/stores/global-dialog'
 
-const globalDialog = useDialog()
+const dialog = useDialog()
 </script>
 
 <style scoped lang="scss"></style>
