79b9d15eec2d64b9c201e0bc897a673d8b91424c
inlym <inlym@qq.com>
2024-09-26 23:38:06 +0800

    chore: wrap http request

--- src/core/http.ts
+++ src/core/http.ts
@@ -1,10 +1,11 @@
-import axios, {type AxiosRequestConfig} from 'axios'
-import {createAliyunApigwSignatureInterceptor} from './axios/aliyunApigwSignatureInterceptor'
-import {authInterceptor} from './axios/authInterceptor'
-import {paramsSerializer} from './axios/paramsSerializer'
-import {handleBusinessError} from './axios/handleBusinessError'
 import {BusinessError} from '@/core/types'
 import {Modal} from 'ant-design-vue'
+import axios, {type AxiosRequestConfig} from 'axios'
+import {useRequest, type Options, type Service} from 'vue-request'
+import {createAliyunApigwSignatureInterceptor} from './axios/aliyunApigwSignatureInterceptor'
+import {authInterceptor} from './axios/authInterceptor'
+import {handleBusinessError} from './axios/handleBusinessError'
+import {paramsSerializer} from './axios/paramsSerializer'
 
 const appKey = import.meta.env.VITE_ALIYUN_APP_KEY
 const appSecret = import.meta.env.VITE_ALIYUN_APP_SECRET
@@ -35,16 +36,36 @@ export async function requestForData<T = any>(config: AxiosRequestConfig): Promi
 }
 
 /**
- * 通用错误处理器（在 VueRequest 中使用）
+ * 发起数据请求
  *
- * ### 说明
- * 为使函数名简单，采用同名方法
+ * ### 使用说明
+ * 当前方法用于在页面层级注册 HTTP 请求
  */
-export function onHttpError(error: Error) {
-  if (error instanceof BusinessError) {
-    // 已经在 axios 拦截器中处理，将错误封装为了 `BusinessError`，因此发生错误一定会进入这一个分支
-    if (!error.handled) {
-      Modal.info({title: '提示', content: error.errorMessage, okText: '我知道了'})
+export function useData<R, P extends unknown[] = any>(service: Service<R, P>, options?: Options<R, P>) {
+  // 对 `options` 进行默认值处理
+  if (options === undefined) {
+    options = {}
+  }
+  if (options.manual === undefined) {
+    options.manual = true
+  }
+  if (options.loadingDelay === undefined) {
+    options.loadingDelay = 500
+  }
+  if (options.loadingKeep === undefined) {
+    options.loadingKeep = 1000
+  }
+  // 发生错误时，自动弹窗提示
+  if (options.onError === undefined) {
+    options.onError = (error: Error) => {
+      if (error instanceof BusinessError) {
+        // 已经在 axios 拦截器中处理，将错误封装为了 `BusinessError`，因此发生错误一定会进入这一个分支
+        if (!error.handled) {
+          Modal.info({title: '提示', content: error.errorMessage, okText: '我知道了'})
+        }
+      }
     }
   }
+
+  return useRequest<R, P>(service, options)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
 import Antd from 'ant-design-vue'
 import {createPinia} from 'pinia'
 import {createApp} from 'vue'
+import Vue3Lottie from 'vue3-lottie'
 import App from './App.vue'
 import router from './router'
-import {setGlobalOptions} from 'vue-request'
-import Vue3Lottie from 'vue3-lottie'
 
 import 'ant-design-vue/dist/reset.css'
 import 'tailwindcss/tailwind.css'
-
-// VueRequest 全局配置
-setGlobalOptions({
-  /** 手动触发请求 */
-  manual: true,
-  /** 延迟的毫秒数 */
-  loadingDelay: 1000,
-  /** 保持 loading 的毫秒数 */
-  loadingKeep: 1000,
-})
 
 const app = createApp(App)
 
