e780c55b49f7a611b429f6335b6a525095839418
inlym <inlym@qq.com>
2024-10-13 13:04:31 +0800

    chore: optimize types and add global variables

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   /** 应用名称 */
   readonly VITE_APP_NAME: string
+  /** 环境名称: 开发、测试、生产 */
+  readonly VITE_ENV_NAME: 'dev' | 'test' | 'prod'
   /** 服务器请求地址 */
   readonly VITE_REQUEST_BASE_URL: string
   /** 阿里云 API 网关摘要签名认证使用的 AppKey */
@@ -14,3 +16,12 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// 以下来源于在 Vite 的 `difene` 配置的全局常量
+// https://cn.vitejs.dev/config/shared-options.html#define
+
+/** 应用版本号 */
+declare const __APP_VERSION__: string
+
+/** 最后一次构建时间 */
+declare const __LAST_BUILD_TIME__: string
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import {defineConfig} from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,4 +28,11 @@ export default defineConfig({
   // 环境变量目录
   // https://cn.vitejs.dev/config/shared-options.html#envdir
   envDir: path.resolve(__dirname, './env'),
+
+  // 定义全局常量
+  // https://cn.vitejs.dev/config/shared-options.html#define
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+    __LAST_BUILD_TIME__: JSON.stringify(new Date().toLocaleString()),
+  },
 })
