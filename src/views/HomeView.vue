457edc4f9bf5a4907228abe7ce6d9299d9df2109
inlym <inlym@qq.com>
2024-10-15 01:12:00 +0800

    refactor: rename `services` to `api`

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,28 +3,9 @@
     <TheHeader />
   </a-affix>
   <div class="h-[1500px] bg-green-300">Content</div>
-  <div class="h-[500px] bg-slate-200" @click="a">Footer</div>
+  <div class="h-[500px] bg-slate-200">Footer</div>
 </template>
 
-<script lang="ts" setup>
-import TheHeader from '@/components/global/TheHeader.vue'
-import {logger} from '@/core/logger'
-import {useCounterStore} from '@/stores/counter'
-import {reactive, ref} from 'vue'
-const counter = useCounterStore()
-console.log(counter.count)
-
-function a() {
-  counter.increment()
-  console.log(counter.count)
-
-  b()
-}
-
-function b() {
-  logger.debug(__APP_VERSION__)
-  logger.info(__LAST_BUILD_TIME__)
-}
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped></style>
