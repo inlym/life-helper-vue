93bf142a485a833b89b3328ba860f25099d85365
inlym <inlym@qq.com>
2024-11-30 22:55:28 +0800

    feat: optimize header component

--- src/components/global/TheHeader.vue
+++ src/components/global/TheHeader.vue
@@ -6,18 +6,20 @@
       <!-- logo 图片 -->
       <img class="mr-2 h-10" src="/images/logo.png" alt="logo" />
       <a-tooltip>
-        <template #title>目前处于公测阶段，将于公测完毕后发布稳定版本</template>
-        <div class="mr-10 mt-2 text-xs font-bold text-red-500">Beta</div>
+        <template #title>目前为 Beta 版本，将于公测完毕后发布稳定版本</template>
+        <div class="mr-10 mt-2 cursor-help">
+          <a-tag color="red">Beta</a-tag>
+        </div>
       </a-tooltip>
       <!-- 自适应宽度的间隔区域 -->
       <div class="flex-1"></div>
       <!-- 菜单组 -->
-      <div class="flex h-full">
-        <div class="mx-4 h-full cursor-pointer border-indigo-500 px-2 pt-4 align-middle text-base hover:border-b-4 hover:font-bold">首页</div>
-        <div class="mx-4 h-full cursor-pointer border-indigo-500 px-2 pt-4 align-middle text-base hover:border-b-4 hover:font-bold">产品</div>
-        <div class="mx-4 h-full cursor-pointer border-indigo-500 px-2 pt-4 align-middle text-base hover:border-b-4 hover:font-bold">帮助与支持</div>
-        <div class="mx-4 h-full cursor-pointer border-indigo-500 px-2 pt-4 align-middle text-base hover:border-b-4 hover:font-bold">关于</div>
-      </div>
+      <a-space>
+        <a-button type="text" size="large">首页</a-button>
+        <a-button type="text" size="large">产品</a-button>
+        <a-button type="text" size="large">帮助与支持</a-button>
+        <a-button type="text" size="large">关于</a-button>
+      </a-space>
     </div>
 
     <!-- 右侧区域 -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
+  <!-- 顶部导航栏 -->
   <a-affix :offset-top="0">
     <div class="flex justify-center bg-white">
       <div class="w-[1200px]"><TheHeader /></div>
     </div>
   </a-affix>
-  <div class="banner"></div>
+
+  <!-- 横幅区域 -->
+  <div class="bg-sky-50">
+    <div class="mx-auto flex h-[600px] w-[1200px] justify-between">
+      <!-- 左侧区域 -->
+      <div class="flex flex-col justify-center">
+        <a-typography-title :level="2">个人日程管理</a-typography-title>
+        <a-typography-text>可互动的日程清单，随时随地管理你的生活</a-typography-text>
+        <a-button class="mt-8" size="large" :block="false">免费使用</a-button>
+      </div>
+      <!-- 右侧区域 -->
+      <img src="/images/banner01.png" alt="banner" />
+    </div>
+  </div>
+
+  <!-- 我们的用户模块 -->
+  <div class="bg-cyan-50">
+    <div class="i mx-auto flex flex-col items-center justify-between p-16">
+      <a-typography-title :level="2">我们的用户</a-typography-title>
+      <a-typography-text>为用户创造更多的价值</a-typography-text>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped>
-.banner {
-  height: 600px;
-  background: rgb(238, 174, 202);
-  background: linear-gradient(90deg, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%);
-}
-</style>
+<style lang="scss" scoped></style>
