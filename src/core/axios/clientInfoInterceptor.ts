33cc4ff0205447677fc58476cabd9432f47334ac
inlym <inlym@qq.com>
2024-12-12 20:05:09 +0800

    feat: optimize client info interceptor

--- src/core/axios/clientInfoInterceptor.ts
+++ src/core/axios/clientInfoInterceptor.ts
@@ -8,8 +8,12 @@ import {COMMIT_ID, COMMIT_NAME} from '../ci'
  * @date 2024/12/12
  */
 export function clientInfoInterceptor(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
+  const type = 'web'
+  const id = window.location.hostname
+  const version = __APP_VERSION__
+
+  config.headers.set('x-weutil-client-info', `type=${type}; id=${id}; version=${version}`)
   config.headers.set('x-weutil-ci-commit', `name=${COMMIT_NAME}; id=${COMMIT_ID}`)
-  config.headers.set('x-weutil-client-version', __APP_VERSION__)
 
   return config
 }
