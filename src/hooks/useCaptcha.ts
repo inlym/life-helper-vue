e6e1330a52c673a57e1c2fa23f0c0ffa9aec92bc
inlym <inlym@qq.com>
2024-12-15 01:02:14 +0800

    refactor: optimize useCaptcha function

--- src/hooks/useCaptcha.ts
+++ src/hooks/useCaptcha.ts
@@ -2,19 +2,17 @@ import {useScriptTag} from '@vueuse/core'
 
 /** 使用验证码方法参数 */
 export interface UseCaptchaOptions {
-  /** 页面上预留的渲染验证码的元素，示例值：`#captcha-element` */
+  /** 页面上预留的渲染验证码的元素，为元素的 id 值，示例值：`#captcha-element` */
   element?: string
-  /** 触发验证码弹窗的元素，示例值：`#captcha-button` */
+  /** 触发验证码弹窗的元素，为元素的 id 值，示例值：`#captcha-button` */
   button?: string
   /** 业务请求(带验证码校验)回调函数 */
   captchaVerifyCallback: (captchaVerifyParam: string) => Promise<{
     captchaResult: boolean
-    bizResult: boolean
+    bizResult?: boolean
   }>
   /** 业务请求结果回调函数 */
-  onBizResultCallback: () => void
-  /** 绑定验证码实例函数 */
-  getInstance: (instance: any) => void
+  onBizResultCallback?: () => void
 }
 
 declare const initAliyunCaptcha: (options: any) => void
@@ -22,20 +20,28 @@ declare const initAliyunCaptcha: (options: any) => void
 /** 动态引入的脚本地址 */
 const scriptUrl = 'https://o.alicdn.com/captcha-frontend/aliyunCaptcha/AliyunCaptcha.js'
 
-/** 使用阿里云验证码 */
+/**
+ * 使用阿里云验证码
+ * @param options 配置参数
+ * @see https://help.aliyun.com/zh/captcha/captcha2-0/user-guide/add-a-web-or-html5-client-to-alibaba-cloud-captcha-2
+ */
 export function useCaptcha(options: UseCaptchaOptions) {
   const {load, unload} = useScriptTag(
     scriptUrl,
     () => {
+      let captcha
+
       initAliyunCaptcha({
         SceneId: import.meta.env.VITE_ALIYUN_CAPTCHA_SCENE_ID,
         prefix: import.meta.env.VITE_ALIYUN_CAPTCHA_PREFIX,
         mode: 'popup',
         element: typeof options.element === 'string' ? options.element : '#captcha-element',
         button: typeof options.button === 'string' ? options.button : '#captcha-button',
-        captchaVerifyCallback: options.captchaVerifyCallback, // 业务请求(带验证码校验)回调函数，无需修改
-        onBizResultCallback: options.onBizResultCallback, // 业务请求结果回调函数，无需修改
-        getInstance: options.getInstance,
+        captchaVerifyCallback: options.captchaVerifyCallback, // 业务请求(带验证码校验)回调函数
+        onBizResultCallback: options.onBizResultCallback, // 业务请求结果回调函数
+        getInstance: (instance: any) => {
+          captcha = instance
+        },
       })
     },
     {manual: true},
