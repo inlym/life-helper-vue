8177bcdaa3723db06f1c14112606fbebf9ce8359
inlym <inlym@qq.com>
2024-12-16 22:01:14 +0800

    feat: modify global config

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 在 ConfigProvider 中配置主题 -->
-  <a-config-provider :theme="themeConfig">
+  <a-config-provider :theme="themeConfig" :locale="zhCN">
     <!-- App 包裹组件，用于提供消费上下文 -->
     <a-app class="h-full w-full">
       <RouterView />
@@ -9,6 +9,7 @@
 </template>
 
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import {storeToRefs} from 'pinia'
 import {RouterView} from 'vue-router'
 import {useAppStore} from './stores/app'
