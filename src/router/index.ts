c562ba78f1f52066a8b44f746ac3172a1ed5c9fb
inlym <inlym@qq.com>
2025-01-26 15:32:38 +0800

    refactor: remove about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -67,15 +67,6 @@ export const router = createRouter({
     },
 
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-
-    {
       path: '/developing',
       name: 'developing',
       component: DevelopingView,
